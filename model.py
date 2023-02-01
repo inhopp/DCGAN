d0cf66561ebac7ea5850e72c212333c41c837363
inhopp <inhonanananna@gmail.com>
2023-02-01 13:48:47 +0900

    update model.py from GAN

---
+++ model.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, channels=1, img_size=32):
+        super(Generator, self).__init__()        
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2 ))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, channels, 3, stride=1, padding=1),
+            nn.Tanh(),
+        )
+
+
+    def forward(self, z):
+        # z = noise vector
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out) 
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channels=1, img_size=32):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filter, out_filter, bn=True):
+            block = [nn.Conv2d(in_filter, out_filter, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+
+            if bn:
+                block.append(nn.BatchNorm2d(out_filter, 0.8))
+            
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        ds_size = img_size // 2 ** 4 # downsampled image size
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+
+        return validity
